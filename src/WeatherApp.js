52122e4446069830498e4134c1ae57df0a9d8abe
iaza18 <zyamka1808@gmail.com>
2022-12-08 00:44:27 +0100

    API added

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,59 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Search from "./Search";
 import "./WeatherApp.css";
 
-export default function WeatherApp() {
-  let weatherData = {
-    city: "Kharkiv",
-    date: "Tuesday, October 20",
-    time: "20:25",
-    description: "Cloudy ",
-    wind: "5",
-    humidity: "80"
-  };
-  return (
-    <div className="WeatherApp">
-        <div className="container">
-      <div className="row">
-        <div className="col-6" id="right-block">
-          <h1 id="current-city">{weatherData.city}</h1>
-          <h6 id="current-date">
-            {weatherData.date} <br /> {weatherData.time}
-          </h6>
-          <div className="current-weather-block">
-            <span className="local-weather">
-              <span id="weather-description">{weatherData.description}</span>
-              <i
-                className="fa-solid fa-cloud-sun float-left"
-                id="weather-icon"
-              ></i>
-            </span>
-            <br />
-            <span className="currentTemp" id="curTemp">
-              19
-            </span>
 
-            <span className="units">
-              <a href="###" id="celsius-temp" className="active">
-                °C
-              </a>{" "}
-              |
-              <a href="###" id="fahrenheit-temp">
-                °F
-              </a>
-            </span>
-            <p className="currentData">
-              Wind: {weatherData.wind} km/h
-              <br />
-              Humidity: {weatherData.humidity}%
-            </p>
-          </div>
-        </div>
-        <div className="col-6" id="left-block">
-          <Search />
-        </div>
-      </div>
-    </div>
-    </div>
-  );
+export default function WeatherApp(props) {
+    const [weatherData, setWeatherData] = useState({ready: false});
+function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+        ready: true,
+        temperature: response.data.main.temp,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+        humidity: response.data.main.humidity,
+        description: response.data.weather[0].description,
+        iconUrl: "http://openweathermap.org/img/wn/$@2x.png",
+        date:  `Tuesday, October 22`,
+        time: `20:25`,
+    });
 }
+
+    if (weatherData.ready) {
+        return (
+            <div className="WeatherApp">
+                <div className="container">
+              <div className="row">
+                <div className="col-6" id="right-block">
+                  <h1 id="current-city">{weatherData.city}</h1>
+                  <h6 id="current-date">
+                    {weatherData.date} <br /> {weatherData.time}
+                  </h6>
+                  <div className="current-weather-block">
+                    <span className="local-weather">
+                      <span id="weather-description">{weatherData.description}</span>
+                      <img
+                      src={weatherData.iconUrl}
+                      alt={weatherData.description}
+                        className="float-left"
+                        id="weather-icon"
+                      />
+                    </span>
+                    <br />
+                    <span className="temperature">
+                      {Math.round(weatherData.temperature)}
+                    </span>
+        
+                    <span className="units">
+                      <a href="###" id="celsius-temp" className="active">
+                        °C
+                      </a>{" "}
+                      |
+                      <a href="###" id="fahrenheit-temp">
+                        °F
+                      </a>
+                    </span>
+                    <p className="currentData">
+                      Wind: {weatherData.wind} km/h
+                      <br />
+                      Humidity: {weatherData.humidity}%
+                    </p>
+                  </div>
+                </div>
+                <div className="col-6" id="left-block">
+                  <Search />
+                </div>
+              </div>
+            </div>
+            </div>
+          );
+    } else {
+        const apiKey= `5f472b7acba333cd8a035ea85a0d4d4c`; 
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+
+      return "Loading..."
+      };
+    }
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <div className="App">
-      <WeatherApp />
+      <WeatherApp defaultCity="Myrhorod"/>
       <CodedBy />
     </div>
   </React.StrictMode>
