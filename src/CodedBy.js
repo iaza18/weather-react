1685973c324d42678b30753f4738418b72f28578
iaza18 <zyamka1808@gmail.com>
2022-12-11 16:01:49 +0100

    Updated github link

--- src/CodedBy.js
+++ src/CodedBy.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CodedBy() {
   return (
     <small>
-      <a href="https://github.com/iaza18/weather-app" className="CodedBy">
+      <a href="https://github.com/iaza18/weather-react" className="CodedBy">
         Open-source code
       </a>{" "}
       by Iana Zavoloka©
